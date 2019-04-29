1a1e1dce7cb476c53652a698d38274a81c2869ae
PJBewty <50085806+PJBewty@users.noreply.github.com>
2019-04-29 15:37:17 +0700

    Add files via upload

--- Search2.php
+++ Search2.php
@@ -1,6 +1,6 @@
 <?php
     require('Connect_DB.php');
-    $sql_text = "SELECT * FROM Linebot1 WHERE Keyword LIKE '%ac%'";
+    $sql_text = "SELECT * FROM Linebot1 WHERE Keyword LIKE 'c'";
     $query = mysqli_query($conn,$sql_text);
     while($objresult = mysqli_fetch_assoc($query))
     {
